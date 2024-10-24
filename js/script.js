1f55dcf0cec3e54a0f4a916d3f1826b8de27a647
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-24 16:13:14 +0200

    aggiunta numeri randm del computer

--- js/script.js
+++ js/script.js
@@ -4,3 +4,29 @@
 //4) far inserire all'utente 5 numeri negli input vuoti che si inseriranno in un array vuoto
 //5) confrontare i due array e verificare quali e quanti numeri coinbacino
 //6) far apparire in pagina il risultato di quest'ultimo calcolo
+
+const inputGroup = document.getElementById('input-group')
+
+const numberList = document.getElementById('number-list')
+
+
+//numeri random da pc
+function getRandomInt(max) {
+    return Math.floor(Math.random() * 50);
+  }
+
+let elementList1 = document.getElementById('element-list-1')
+elementList1.innerHTML = `${Math.floor(Math.random() * 50)}`
+
+let elementList2 = document.getElementById('element-list-2')
+elementList2.innerHTML = `${Math.floor(Math.random() * 50)}`
+
+let elementList3 = document.getElementById('element-list-3')
+elementList3.innerHTML = `${Math.floor(Math.random() * 50)}`
+
+let elementList4 = document.getElementById('element-list-4')
+elementList4.innerHTML = `${Math.floor(Math.random() * 50)}`
+
+let elementList5 = document.getElementById('element-list-5')
+elementList5.innerHTML = `${Math.floor(Math.random() * 50)}`
+
