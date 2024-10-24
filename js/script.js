cd0a19c0be53bbd1745076cbaf9c6eab54f38ecf
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-24 14:10:54 +0200

    creazione file e ragionamento sui procedimenti da seguire

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//1) Creo un array di partenza in cui verrano pushati 5 numeri casuali
+//2) far partire un contatore di 30 secondi 
+//3) far sparire l'array di partenza, lasciando in pagina 5 input vuoti
+//4) far inserire all'utente 5 numeri negli input vuoti che si inseriranno in un array vuoto
+//5) confrontare i due array e verificare quali e quanti numeri coinbacino
+//6) far apparire in pagina il risultato di quest'ultimo calcolo
